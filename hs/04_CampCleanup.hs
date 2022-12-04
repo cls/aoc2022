da06cab8db152d4dbadb213eb5f0cccb028b8e74
Connor Lane Smith <cls@lubutu.com>
2022-12-04 09:19:19 +0000

    Day 04 - Camp Cleanup

---
+++ hs/04_CampCleanup.hs
@@ -0,0 +1,38 @@
+main :: IO ()
+main = do input <- map (pair . read) <$> lines <$> getContents
+          print . part1 $ input
+          print . part2 $ input
+
+{- Part 1 -}
+
+data Range = Range Int Int
+
+data Pair = Pair { pair :: (Range, Range) }
+
+instance Read Range where
+  readsPrec _ s = [(Range x y, u) | (x, '-':t) <- reads s,
+                                    (y,     u) <- reads t]
+
+instance Read Pair where
+  readsPrec _ s = [(Pair (x, y), u) | (x, ',':t) <- reads s,
+                                      (y,     u) <- reads t]
+
+fullyContains :: Range -> Range -> Bool
+Range a b `fullyContains` Range c d = a <= c && b >= d
+
+fullyOverlaps :: Range -> Range -> Bool
+x `fullyOverlaps` y = x `fullyContains` y || y `fullyContains` x
+
+part1 :: [(Range, Range)] -> Int
+part1 = sum . map (fromEnum . uncurry fullyOverlaps)
+
+{- Part 2 -}
+
+partlyContains :: Range -> Range -> Bool
+Range a b `partlyContains` Range c d = (a <= c && b >= c) || (a <= d && b >= d)
+
+partlyOverlaps :: Range -> Range -> Bool
+x `partlyOverlaps` y = x `partlyContains` y || y `partlyContains` x
+
+part2 :: [(Range, Range)] -> Int
+part2 = sum . map (fromEnum . uncurry partlyOverlaps)
